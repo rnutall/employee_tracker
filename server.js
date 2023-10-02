8ea3288a9c457f1b5977ff9703e05c6089d730a7
rnutall <rnutall1@gmail.com>
2023-10-02 12:58:26 -0500

    tring to fix node error

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const inquirer = require("inquirer");
 // Import and require mysql2
 const mysql = require("mysql2");
-const Connection = require("mysql2/typings/mysql/lib/Connection");
-
+const prompts = require("./assets/config.js");
 // Connect to database
 const db = mysql.createConnection(
   {
@@ -13,7 +12,7 @@ const db = mysql.createConnection(
     database: "employee_db",
   });
 
-  Connection.connect((error) => {
+  db.connect((error) => {
     if (error) throw error;
   
   console.log('Connected to the employee_db database!');

ef1fc013bcc6af4229800b72e1a78588e6063097
rnutall <rnutall1@gmail.com>
2023-10-02 16:50:36 -0500

    added connetion.js and prompt.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 // Import and require mysql2
 const mysql = require("mysql2");
-const prompts = require("./assets/config.js");
+const prompts = require("./config.js");
 // Connect to database
 const db = mysql.createConnection(
   {
